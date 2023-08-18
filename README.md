c48f5900e039c8ccf9283ab5936345abeca425b8
nileblack <nile.black@gmail.com>
2023-08-18 11:06:27 +0800

    protoc 3.18.0-SNAPSHOT

---
+++ README.md
@@ -0,0 +1,4 @@
+## HOW TO COMPILE
+```java
+mavan compile -Dprotoc.path=path.to.protoc
+```
---
+++ src/main/java/in/code123/prometheus/remote/write/Version.java
@@ -0,0 +1,5 @@
+package in.code123.prometheus.remote.write;
+
+public class Version {
+    public static final String VERSION = "3.18.0";
+}
